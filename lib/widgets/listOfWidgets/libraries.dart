55d8896439b810d11d5f0040993810da3dc367c0
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-04-26 16:40:00 +0300

    added libraries

--- lib/widgets/listOfWidgets/libraries.dart
+++ lib/widgets/listOfWidgets/libraries.dart
@@ -1,44 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:tme_ard_v2/widgets/reusable/animatedContainer.dart';
 
-class Libraries extends StatelessWidget {
+class Libraries extends StatefulWidget {
 
+  @override
+  _LibrariesState createState() => _LibrariesState();
+}
+
+class _LibrariesState extends State<Libraries> {
   @override
   Widget build(BuildContext context) {
     final Size screensize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.teal[50],
-      appBar: PreferredSize(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 25,),
-          child: Container(
-            color: Colors.transparent,
-            child: Row(
-              children: <Widget>[
-                Container(
-                  width: 80,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.teal,
-                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(30))
-                  ),
-                  child: IconButton(
-                    icon: Icon(Icons.arrow_back), 
-                    onPressed: (){},
-                    color: Colors.white,
-                    ),
+      body: Stack(
+        children: <Widget>[
+          
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 10),
+            child: Center(
+              child: SingleChildScrollView(
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    AnimatedContainerView(
+                      tittle: "Extensible hd44780 LCD library",
+                      subtittle: "library for hd44780 based LCD display",
+                      author: "Bill Perry",
+                      projectsite: "www.github.com/duinoWitchery/hd44780",
+                      licence: "GPL",
+                      ),
+                    AnimatedContainerView(
+                      tittle: "Adafriut_SSD1306",
+                      subtittle: "library for our Monochrome OLEDs based on SSD1306 drivers",
+                      author: "firma Adafruit",
+                      projectsite: "www.github.com/duinoWitchery/hd44780",
+                      licence: "BSD",
+                      ),
+                    AnimatedContainerView(
+                      tittle: "Adafruit_NeoPixel",
+                      subtittle: "library for controlling single-wire-based LED pixels",
+                      author: "firma Adafruit",
+                      projectsite: "www.github.com/adafruit/Adafruit_NeoPixel",
+                      licence: "GPL",
+                      ),
+                    AnimatedContainerView(
+                      tittle: "Arduino RC5 remote control decoder",
+                      subtittle: "library for decoding infrared remote control",
+                      author: "Bill Perry",
+                      projectsite: "www.github.com/guyc/RC5",
+                      licence: "BSD",
+                      ),
+                    AnimatedContainerView(
+                      tittle: "Adafruit-MCP23008-library",
+                      subtittle: "library for the MCP23008 I2C I/O expander",
+                      author: "firma Adafruit",
+                      projectsite: "www.github.com/duinoWitchery/hd44780",
+                      licence: "BSD",
+                      ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ), 
-        preferredSize: Size(100,60)
-        ),
-          body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text("sample text"),
-          ],
-        )
+
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            width: screensize.width * 0.15,
+            height: screensize.width * 0.15,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(bottomRight: Radius.circular(40)),
+              color: Colors.teal,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 20,
+                  color: Colors.grey,
+                  spreadRadius: 1,
+                  offset: Offset(1, 5)
+                ),
+              ]
+            ),
+            child: IconButton(
+              onPressed: (){
+                
+              }, 
+              icon: Icon(Icons.arrow_back, size: 20,color: Colors.white,),
+              ),
+          ),
+
+
+        ],
       ),
     );
   }
---
+++ lib/widgets/reusable/animatedContainer.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../app_theme.dart';
+
+class AnimatedContainerView extends StatefulWidget {
+
+  final String tittle;
+  final String subtittle;
+  final String projectsite;
+  final String author;
+  final String licence;
+
+  AnimatedContainerView({this.author, this.projectsite, this.subtittle, this.tittle, this.licence}){}
+  
+  @override
+  _AnimatedContainerViewState createState() => _AnimatedContainerViewState();
+}
+
+class _AnimatedContainerViewState extends State<AnimatedContainerView> {
+  bool selected = true;
+  
+  //String site = widget.projectsite;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical:8.0),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                      selected = !selected;
+                      });
+            },
+            child: AnimatedContainer(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: selected ? Colors.indigo[100] : Colors.teal[100],
+              ),
+              curve: !selected ? ElasticOutCurve(.9) : Curves.elasticOut,
+              width: 200.0,
+              height: selected ? 60.0 : 200.0,
+              
+              alignment: selected ? Alignment.center : AlignmentDirectional.topCenter,
+              duration: selected ? Duration(seconds: 3): Duration(seconds: 1),
+              child: !selected ? SingleChildScrollView(
+                        child: Column(  
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.teal),
+                          
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(widget.tittle, style: AppTheme.body1.copyWith(
+                            fontSize: 18,
+                            
+                          ),),
+                        ),
+                      ),
+                    ),
+                    Text("${widget.subtittle}"),
+                    Text("Author - ${widget.author}"),
+                    Text("Licence - ${widget.licence}"),
+                    FlatButton(
+                      onPressed: ()async {
+                      var url = '${widget.projectsite}';
+                      if (await canLaunch(url)) {
+                        await launch(url, forceSafariVC: false);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                      child: Text("Visit offcial site", style: TextStyle(color: Colors.teal[900]),),
+                      
+                      )
+
+                  ],
+                ),
+              ) : Column(
+                children: <Widget>[
+                  Spacer(),
+                  Text(widget.tittle, style: AppTheme.body1,),
+                  Spacer(),
+                ],
+              ),
+            ),
+          ),
+        )
+      );
+  }
+}
